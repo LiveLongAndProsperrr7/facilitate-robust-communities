74e1ad8e8c16c300e0dfb63e7ef412d998e9872d
LiveLongAndProsperrr7 <157714423+LiveLongAndProsperrr7@users.noreply.github.com>
2024-03-26 17:34:59 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    for (let i = 0; i < 10; i++) {
+
+    }
+    const data = [];
+    const data = generateRandomData();
+function main() {
+main();
+
+    return data;
+    });
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    data.forEach(item => {
